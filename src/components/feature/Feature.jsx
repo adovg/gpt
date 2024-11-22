41508f2b9c4ba9872f469d6dcac9ff160297c7ab
adovg <kroatoan@i.ua>
2024-11-22 18:59:02 +0200

    add feature section and redux store

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -4,13 +4,13 @@ import './feature.css';
 const Feature = ({title, text}) => {
     return (
         <div className='gpt3__features-container__feature'>
-            <div className='gpt3__features-container__feature'>
-             <></>
-             <h1>{title}</h1>
-             <div className='gpt3__features-container__feature-text'>
-                {text}
-             </div>
+            <div className='gpt3__features-container__feature-title'>
+                 <div />
+                <h1>{title}</h1>
             </div>
+             <div className='gpt3__features-container__feature-text'>
+                <p>{text}</p>
+             </div>
         </div>
     )
 }
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { useSelector} from 'react-redux';
 import './whatGPT3.css';
 import { Feature } from '../../components';
 
 export const WhatGPT3 = () => {
+
+  const featureTitles = useSelector( state => state.featureTitle);
+  const featureContent = useSelector(state => state.featureContent);
+
+
   return (
-    <div className='gpt3__whatgpt3 section__padding'>
-      <div className="gpt3__whatgpt3-features">
-        <Feature  title='What is GPT-3'/>  
+    <div className='gpt3__whatgpt3 section__margin'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title={featureTitles[0].title} text={featureTitles[0].text} />
       </div>  
       <div className="gpt3__whatgpt3-heading">
         <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-         <Feature /> 
-         <Feature />
-         <Feature />  
+          {featureContent.map((content, key) => ( 
+            <Feature title={content.title} text={content.text} key={key} /> )
+            )}
       </div>
     </div>
   )
 }
 
 export default WhatGPT3;
+
