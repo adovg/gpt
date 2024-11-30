edf00675304474d3d141363e032cb2df573c4546
adovg <kroatoan@i.ua>
2024-11-30 13:16:33 +0200

    CTA section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -10,10 +10,10 @@ export const Possibility = () => {
       </div>
       <div className='gpt3__possibility-content'>
         <h4>Request Early Access to Get Started</h4>
-        <h1 className='gradient-text'>
+        <h1 className='gradient__text'>
           The possibilities are beyond your imagination
         </h1>
-        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.
+        <p className='gpt3__possibility-content-text'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.
            Party we years to order allow asked of.
         </p>
         <p className='gradient__subtext'>
